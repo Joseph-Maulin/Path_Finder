22e75c1f6814ab4fa3effff85a46b6ea692bb9a7
Joseph-Maulin <joseph.l.maulin@gmail.com>
2020-10-19 11:52:22 -0400

    build_1.0

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from path_finder import Path_Finder
+from functools import reduce
 
 
 
 app = Flask(__name__)
 
+pf = Path_Finder()
+
 @app.route('/', methods = ['GET', 'POST'])
 def path_display():
 
-    pf = Path_Finder()
 
-    return render_template("maze_display.html", path_finder = pf)
+    return render_template("maze_display.html")
 
 
+@app.route('/solve', methods = ['POST'])
+def solve_maze():
+    data = dict(request.form)
+
+    pf.set_string_maze(data['arr'])
+    pf.find_path()
+
+    flat_locations = reduce(lambda z, y :z + y, reduce(lambda z, y :z + y, pf.locations.values()))
+    return render_template("maze_display_solved.html", maze_solved=pf.array_maze, locations=flat_locations)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
--- path_finder.py
+++ path_finder.py
@@ -1,49 +1,55 @@
-
+from functools import reduce
+import numpy as np
 
 class Path_Finder:
 
     def __init__(self):
         self.maze = None
         self.array_maze = []
+        self.paths = {}
 
     def set_array_maze(self, maze):
         self.array_maze = maze
+        self.n = len(self.array_maze)
 
-    def set_maze(self, maze):
-        self.maze = self.maze.split()
+    def set_string_maze(self, maze):
+        self.maze = maze.split()
         self.array_maze = []
-        locations = {}
+        self.locations = {}
 
+        finish = False
         for i in range(len(self.maze)):
             new_line = []
             for j in range(len(self.maze[i])):
                 new_line.append(self.maze[i][j])
                 if self.maze[i][j] == '0':
-                    locations[0] = [[i,j]]
+                    self.locations[0] = [[i,j]]
+                elif self.maze[i][j] == 'F':
+                    finish = True
             self.array_maze.append(new_line)
 
-        n = len(self.array_maze)
-        if not locations:
-            locations[0] = [[0,0]]
+        self.n = max(len(self.array_maze), len(self.array_maze[0]))
+        if not self.locations:
+            self.locations[0] = [[0,0]]
             self.array_maze[0][0] = 0
 
-    def find_path(self, maze):
-        self.array_maze = maze
-        locations = {0:[[0,0]]}
-        self.array_maze[0][0] = 0
+        if not finish:
+            self.array_maze[-1][-1] = 'F'
+
+    def find_path(self):
 
         i = 0
         while True:
-            if not locations[i]:
+            if not self.locations[i]:
                 return False
-            for loc in locations[i]:
-                if not i+1 in locations.keys():
-                    locations[i+1] = []
-                for step in self.find_steps(loc, n):
+            for loc in self.locations[i]:
+                if not i+1 in self.locations.keys():
+                    self.locations[i+1] = []
+                for step in self.find_steps(loc, self.n):
                     if step == 'F':
                         return True
                     self.array_maze[step[0]][step[1]] = i+1
-                    locations[i+1].append(step)
+                    self.locations[i+1].append(step)
 
             # self.print_maze()
             i+=1
@@ -60,7 +66,7 @@ class Path_Finder:
                 r_locs.append([loc[0]-1, loc[1]])
 
         #right
-        if loc[1] < n-1:
+        if loc[1] + 1 < len(self.array_maze[0]):
             right = self.array_maze[loc[0]][loc[1]+1]
             if right == 'F':
                 return ['F']
@@ -68,7 +74,7 @@ class Path_Finder:
                 r_locs.append([loc[0], loc[1]+1])
 
         # down
-        if loc[0] < n-1:
+        if loc[0]+1 < len(self.array_maze):
             down = self.array_maze[loc[0]+1][loc[1]]
             if down == 'F':
                 return ['F']
@@ -95,15 +101,16 @@ class Path_Finder:
 if __name__ == "__main__":
 
     d = "\n".join([
-      ".W.F.." + ".W...." * 5,
-      ".W...." * 6,
-      ".W.WW." * 6,
-      "....W." * 6,
-      "....W." * 6,
-      "....W." * 5 + "....W0"
+      ".W....",
+      ".W....",
+      ".W.WW.",
+      "....W.",
+      "....W."
     ])
 
 
     pf = Path_Finder()
-    pf.set_maze(d)
+    pf.set_string_maze(d)
     pf.find_path()
+    pf.print_maze()
+    print(reduce(lambda z, y :z + y, reduce(lambda z, y :z + y, pf.locations.values())))
