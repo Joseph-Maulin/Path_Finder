99625d395ca0c196003b146f8d9d5ce786240ab2
Joseph-Maulin <joseph.l.maulin@gmail.com>
2020-10-29 14:13:29 -0400

    added final path draw

--- path_finder.py
+++ path_finder.py
@@ -6,7 +6,6 @@ class Path_Finder:
     def __init__(self):
         self.maze = None
         self.array_maze = []
-        self.paths = {}
 
     def set_array_maze(self, maze):
         self.array_maze = maze
@@ -37,19 +36,55 @@ class Path_Finder:
             self.array_maze[-1][-1] = 'F'
 
     def find_path(self):
-
+        self.path_tracks = [[[0,0]]]
         i = 0
         while True:
             if not self.locations[i]:
                 return False
             for loc in self.locations[i]:
+
+                # [[0,0], [0,1], ..]
+                # [[0,0], [1,0], ..]
+                # if self.find_steps == None
+                # delete paths with that step in it
+                # else copy path with that step for all find_steps found and add onto it
+
+
                 if not i+1 in self.locations.keys():
                     self.locations[i+1] = []
-                for step in self.find_steps(loc, self.n):
-                    if step == 'F':
-                        return True
-                    self.array_maze[step[0]][step[1]] = i+1
-                    self.locations[i+1].append(step)
+
+                steps = self.find_steps(loc, self.n)
+
+                # print(self.path_tracks)
+
+                if len(steps) == 0:
+                    t = []
+                    for path in self.path_tracks:
+                        if loc not in path:
+                            t.append(path)
+                    self.path_tracks = t[::]
+
+                else:
+                    t = []
+                    paths_to_duplicate = []
+                    for path in self.path_tracks:
+                        if loc in path:
+                            paths_to_duplicate.append(path)
+                        else:
+                            t.append(path)
+                    self.path_tracks = t[::]
+
+                    for step in steps:
+                        if step == 'F':
+                            self.path = np.array(paths_to_duplicate[0]).flatten().tolist()
+                            return True
+
+                        for path in paths_to_duplicate:
+                            route = path + [step]
+                            self.path_tracks.append(route)
+
+                        self.array_maze[step[0]][step[1]] = i+1
+                        self.locations[i+1].append(step)
 
             # self.print_maze()
             i+=1
@@ -101,16 +136,17 @@ class Path_Finder:
 if __name__ == "__main__":
 
     d = "\n".join([
-      ".W....",
-      ".W....",
-      ".W.WW.",
-      "....W.",
-      "....W."
+      ".W...." * 10,
+      ".W...." * 10,
+      ".W.WW." * 10,
+      "....W." * 10,
+      "....W." *10,
     ])
 
 
     pf = Path_Finder()
     pf.set_string_maze(d)
     pf.find_path()
-    pf.print_maze()
-    print(reduce(lambda z, y :z + y, reduce(lambda z, y :z + y, pf.locations.values())))
+    # pf.print_maze()
+    print(pf.path)
+    # print(reduce(lambda z, y :z + y, reduce(lambda z, y :z + y, pf.locations.values())))
